32a1340a0647717a71853caef7f1433980f709e2
Garrett Richards <grichards@mkone.co>
2015-10-21 00:16:33 -0700

    initial commit

---
+++ instacopy/instacopy/CPYLoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CPYLoginViewController.swift
+//  instacopy
+//
+//  Created by Garrett Richards on 10/20/15.
+//  Copyright © 2015 Acme. All rights reserved.
+//
+
+import UIKit
+
+class CPYLoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func actionLogin(sender: UIButton) {
+    
+    }
+}
---
+++ instacopy/instacopy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instacopy
+//
+//  Created by Garrett Richards on 10/20/15.
+//  Copyright © 2015 Acme. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
+
